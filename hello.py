433158342813e120db0c4e5d796bc5a6602cd24d
saryuhan-logeswaran <saryuhan.logeswaran@gmail.com>
2023-09-23 16:40:42 -0400

    Reproduced example 4-7

--- hello.py
+++ hello.py
@@ -1,21 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, flash
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 from flask_bootstrap import Bootstrap
-from flask_moment import Moment
-from datetime import datetime
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
+
 bootstrap = Bootstrap(app)
-moment = Moment(app)
 
-# @app.route('/')
-# def index():
-#     return '<h1>Hello World!</h1>'
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
 
-# @app.route('/user/<name>')
-# def user(name):
-#     return '<h1>Hello, {}!</h1>'.format(name)
-
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', current_time=datetime.utcnow())
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+        session['name'] = form.name.data
+
+    return render_template('index.html', form=form, name=session.get('name'))
